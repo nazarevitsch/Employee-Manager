03a9cdd42b2e668e2362d97979df209953bb71f3
nazarevitsch <bidaritter1997@gmail.com>
2022-05-02 19:25:54 +0300

    Task (entity and db) was added.

---
+++ src/main/java/com/bida/employer/manager/domain/Task.java
@@ -0,0 +1,37 @@
+package com.bida.employer.manager.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @Column(name = "t_id")
+    @Type(type = "pg-uuid")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    @Column(name = "t_title")
+    private String title;
+
+    @Column(name = "t_description")
+    private String description;
+
+    @Column(name = "t_task_time")
+    private LocalDateTime taskTime;
+
+    @Column(name = "t_shift_id")
+    private UUID shift_id;
+}
